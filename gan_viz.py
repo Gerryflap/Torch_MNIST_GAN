16c47a5077743bdb5b07363ba571cf209b6131dd
gerben <gerry2211@gmail.com>
2019-10-14 16:32:02 +0200

    Added file picking dialog

--- gan_viz.py
+++ gan_viz.py
@@ -2,22 +2,28 @@ import os
 import torch
 import numpy as np
 from PIL import Image, ImageTk
+from tkinter import filedialog
 import tkinter as tk
-from mnist_gan import MnistGenerator, MnistDiscriminator
+from mnist_gan import MnistGenerator, MnistDiscriminator, mish
 
 image = None
 orig_img = None
 should_update = True
 
-generator = torch.load("./saved_models/run_1/generator.pt", map_location=torch.device('cpu'))
-discriminator = torch.load("./saved_models/run_1/discriminator.pt", map_location=torch.device('cpu'))
+root = tk.Tk()
+filename = filedialog.askopenfilename(initialdir="./saved_models", title="Select generator",
+                                           filetypes=(("Pytorch model", "*.pt"), ("all files", "*.*")))
+
+generator = torch.load(filename, map_location=torch.device('cpu'))
 
 z_shape = int(generator.latent_size)
 z = np.zeros((1, z_shape), dtype=np.float32)
 
+
 def set_should_update():
     global should_update
     should_update = True
+
 
 def randomize():
     global should_update
@@ -38,6 +44,7 @@ def dankify():
     should_update = False
     for slider in sliders:
         slider.set(4.20)
+
 
 def load_z():
     global should_update
@@ -67,7 +74,7 @@ def update_canvas(iets):
     array = generator(torch.from_numpy(z))[0]
     if array.shape[0] == 1:
         array = array[0, :, :]
-    array = (array + 1)/2
+    array = (array + 1) / 2
     array *= 255.0
     array = array.detach().numpy().astype(np.uint8)
     img = Image.fromarray(array)
@@ -77,7 +84,7 @@ def update_canvas(iets):
     image = img
     canvas.create_image(0, 0, anchor="nw", image=image)
 
-root = tk.Tk()
+
 root.title("GAN tool")
 root.attributes('-type', 'dialog')
 
@@ -94,7 +101,7 @@ def scroll_set(*args):
     scrollbar.set(*args)
 
 
-subcanvas = tk.Canvas(root,  bd=0, highlightthickness=0, yscrollcommand=scroll_set, width=500, height=500)
+subcanvas = tk.Canvas(root, bd=0, highlightthickness=0, yscrollcommand=scroll_set, width=500, height=500)
 subframe = tk.Frame(subcanvas)
 
 # for i, slider in enumerate(sliders):
@@ -102,8 +109,9 @@ subframe = tk.Frame(subcanvas)
 # subframe.pack(side=tk.LEFT, fill=tk.BOTH)
 subcanvas.pack(side=tk.RIGHT)
 scrollbar.config(command=subcanvas.yview)
-subframe_id = subcanvas.create_window((0,0), window=subframe, anchor='nw')
-sliders = [tk.Scale(subframe, from_=-5.0, to_=5.0, resolution=0.05, length=290, orient=tk.HORIZONTAL, command=update_canvas if i != z_shape-1 else update_and_enable_updates) for i in range(z_shape)]
+subframe_id = subcanvas.create_window((0, 0), window=subframe, anchor='nw')
+sliders = [tk.Scale(subframe, from_=-5.0, to_=5.0, resolution=0.05, length=290, orient=tk.HORIZONTAL,
+                    command=update_canvas if i != z_shape - 1 else update_and_enable_updates) for i in range(z_shape)]
 list(map(lambda s: s.pack(), sliders))
 
 
@@ -131,7 +139,7 @@ update_canvas(None)
 reset_button = tk.Button(left_frame, text="Reset", command=reset)
 reset_button.pack()
 
-save_button = tk.Button(left_frame, text="Save", command=lambda:  orig_img.save("saved_image.png"))
+save_button = tk.Button(left_frame, text="Save", command=lambda: orig_img.save("saved_image.png"))
 save_button.pack()
 
 randomize_button = tk.Button(left_frame, text="Randomize", command=randomize)
@@ -144,7 +152,5 @@ load_z_button = tk.Button(left_frame, text="Load_z", command=load_z)
 load_z_button.pack()
 
 left_frame.pack(side=tk.LEFT)
-
-
 
 tk.mainloop()
